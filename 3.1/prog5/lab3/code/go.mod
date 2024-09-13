166c725dc2554ca36f3da9b163eb3e9ff272c656
webbsalad <webbbsalad@gmail.com>
2024-09-13 14:08:42 +0300

    add: prog5/lab3

---
+++ 3.1/prog5/lab3/code/go.mod
@@ -0,0 +1,9 @@
+module lab3
+
+go 1.22.2
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/webbsalad/go-weather-bot v1.0.0 // indirect
+)
---
+++ 3.1/prog5/lab3/code/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/webbsalad/go-weather-bot/bot"
+	"github.com/webbsalad/go-weather-bot/config"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b := bot.NewBot(cfg.TelegramToken)
+	b.Start()
+
+}
