79127deea802a6f22a98c2c4c5fa7edb611d36f1
webbsalad <webbbsalad@gmail.com>
2024-10-03 10:35:16 +0300

    feat: prog/lab5

--- 3.1/prog5/lab5/code/main.py
+++ 3.1/prog5/lab5/code/main.py
@@ -3,6 +3,7 @@ from xml.etree import ElementTree as ET
 import time
 import matplotlib.pyplot as plt
 
+
 class SingletonMeta(type):
     _instances = {}
 
@@ -12,37 +13,50 @@ class SingletonMeta(type):
             cls._instances[cls] = instance
         return cls._instances[cls]
 
-class Currencies(metaclass=SingletonMeta):
+
+class CurrencyFetcher(metaclass=SingletonMeta):
     def __init__(self):
-        self.__currencies = []
+        self.last_request_time = 0
+        self.currencies = []
 
     def get_currencies(self, currencies_ids_lst):
-        cur_res_str = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-        result = []
+        current_time = time.time()
+        if current_time - self.last_request_time < 1:
+            raise Exception("Запросы можно делать не чаще, чем раз в секунду.")
+        self.last_request_time = current_time
 
-        for _v in ET.fromstring(cur_res_str.content).findall("Valute"):
-            valute_id = _v.get('ID')
-            if str(valute_id) in currencies_ids_lst:
-                valute_cur_name = _v.find('Name').text
-                valute_cur_val = _v.find('Value').text.replace(',', '.')
-                valute_charcode = _v.find('CharCode').text
-                result.append({valute_charcode: (valute_cur_name, valute_cur_val)})
-
-        self.__currencies = result
+        response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+        root = ET.fromstring(response.content)
+        self.currencies = []
+        for valute in root.findall('Valute'):
+            valute_id = valute.get('ID')
+            if valute_id in currencies_ids_lst:
+                name = valute.find('Name').text
+                value = valute.find('Value').text.replace(',', '.')
+                charcode = valute.find('CharCode').text
+                nominal = int(valute.find('Nominal').text)
+                value_per_unit = float(value) / nominal
+                self.currencies.append({charcode: (name, f"{value_per_unit:.4f}")})
+        return self.currencies
 
     def visualize_currencies(self):
-        cur_codes = [list(currency.keys())[0] for currency in self.__currencies]
-        cur_values = [float(list(currency.values())[0][1]) for currency in self.__currencies]
+        fig, ax = plt.subplots()
+        charcodes = [list(item.keys())[0] for item in self.currencies]
+        values = [float(list(item.values())[0][1]) for item in self.currencies]
 
-        plt.figure(figsize=(12, 6))
-        plt.bar(cur_codes, cur_values, color='skyblue')
-        plt.xlabel('Currency Code')
-        plt.ylabel('Exchange Rate')
-        plt.title('Exchange Rates')
+        ax.bar(charcodes, values, color='blue')
+        ax.set_xlabel('Валюта')
+        ax.set_ylabel('Курс (в рублях)')
+        ax.set_title('Курсы валют')
         plt.savefig('currencies.jpg')
         plt.show()
 
+
 if __name__ == '__main__':
-    currencies_obj = Currencies()
-    currencies_obj.get_currencies(['R01035', 'R01335', 'R01700J'])
-    currencies_obj.visualize_currencies()
+    fetcher = CurrencyFetcher()
+    try:
+        result = fetcher.get_currencies(['R01035', 'R01335', 'R01700J'])
+        print(result)
+        fetcher.visualize_currencies()
+    except Exception as e:
+        print(e)
